18bf8ce61a7ac9ed56b9c798894e364d066a6351
mdaurangzeb398 <mdaurangzeb398@gmail.com>
2025-07-08 19:25:57 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const KisswaarApp());
+}
+
+class KisswaarApp extends StatelessWidget {
+  const KisswaarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kisswaar 💘',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.pinkAccent,
+        fontFamily: 'Roboto',
+      ),
+      home: const HomePage(),
+    );
+  }
+}
