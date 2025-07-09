f5953b6ed26e145359a43f3cb5797d6cd062b541
mdaurangzeb398 <mdaurangzeb398@gmail.com>
2025-07-09 13:30:31 +0530

    Create capsule_vibe_dance_trigger.dart

---
+++ capsule_vibe_dance_trigger.dart
@@ -0,0 +1,9 @@
+String getCapsuleDanceEmoji(String capsuleType) {
+  switch (capsuleType) {
+    case 'Streetwear': return '🧢💃🔥';
+    case 'Minimalist': return '🌿🕺💫';
+    case 'Monochrome': return '🖤🎩🌀';
+    case 'Athleisure': return '🏃🎽🎧';
+    default: return '🛍️🕺🎉';
+  }
+}
